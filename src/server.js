efadf142aeeeb9f4bea3f513b5ccc3e4d9f3fec6
nddmanh <manhnd.ilec@gmail.com>
2021-06-11 20:43:16 +0700

    [1.0.0] connect mongoDB

--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
 import express from "express";
+import ConnectDB from "./config/connectDB";
+import ContactModel from "./models/contact.model";
+
 let app = express();
 
-let hostname = "localhost";
-let port = 8017;
+ConnectDB();
 
-app.get("/helloworld", (req, res) => {
-    res.send("<h1> Hello World </h1>");
+app.get("/test-db", async (req, res) => {
+    try {
+        let item = {
+            userId: "174589632",
+            contactId: "4aaaa8952",
+        };
+        let contact = await ContactModel.createNew(item);
+        res.send(contact);
+    } catch (err) {
+        console.log(err);
+    }
 });
 
-app.listen(port, hostname, () =>  {
-    console.log(`Xin chao Duc Manh, server running at ${hostname}:${port}`);
+app.listen(process.env.APP_PORT , process.env.APP_HOST , () =>  {
+    console.log(`Xin chao Duc Manh, server running at ${process.env.APP_HOST }:${process.env.APP_PORT }`);
 })
