2ce7d824808d752593f19124fbc6ed8cd3be166c
nddmanh <manhnd.ilec@gmail.com>
2021-06-11 12:05:16 +0700

    [1.0.0] Define model schemas

---
+++ src/models/chatGroup.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+let Schema = mongoose.Schema;
+
+let ChatGroupSchema = new Schema({
+    name: String,
+    userAmount: {type: Number, min: 3, max: 100},
+    messageAmount: {type: Number, default: 0},
+    userId: String,
+    member: [
+        {userId: String}
+    ],
+    createdAt: {type: Number, default: Date.now},
+    updateAt: {type: Number, default: null},
+    deleteAt: {type: Number, default: null}
+});
+
+module.exports = mongoose.model("chat-group", ChatGroupSchema);
